5cc19427ed6fafd6eba86e2704038c257e7b4a1a
Big Choi <4bigchoi23@gmail.com>
2021-06-30 14:37:28 +0900

    Create user.js

---
+++ user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://glue.cf/
+// @version      1
+// @description  try to take over the world!
+// @author       Alex (colortable@gmail.com)
+// @match        <$URL$>
+// @require      https://code.jquery.com/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function ($, undefined) {
+    // Your code here...
+})(window.jQuery.noConflict(true));
